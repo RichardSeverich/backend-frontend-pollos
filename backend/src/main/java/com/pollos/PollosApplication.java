55f7e2d1d7da2546c6c8b8ea55a2538d266644fc
10micky10 <10miguel94@gmail.com>
2021-03-23 21:59:24 -0400

    init spring project with mvnw and gradle

---
+++ backend/src/main/java/com/pollos/PollosApplication.java
@@ -0,0 +1,13 @@
+package com.pollos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollosApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PollosApplication.class, args);
+  }
+
+}
